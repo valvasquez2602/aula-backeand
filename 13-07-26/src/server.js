ae414d634d05c230207455ea315f565f6f89073f
valvasquez2602 <vasquezlopezvaleria434@gmail.com>
2026-07-20 16:21:10 -0300

    Update server.js

--- 13-07-26/src/server.js
+++ 13-07-26/src/server.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import routes from './router/Motoristas.route.js';
+
 const app = express();
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({ status: "API Online", message: "Acesse /Motoristas para ver os dados" });
+});
+
 app.use('/Motoristas', routes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
-
-
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
