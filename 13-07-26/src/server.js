6f3aa204c0ada20252fab85dd7c066ccce6e8d5e
valvasquez2602 <vasquezlopezvaleria434@gmail.com>
2026-07-20 16:15:40 -0300

    atualizando

--- 13-07-26/src/server.js
+++ 13-07-26/src/server.js
@@ -9,3 +9,10 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+export default app;
